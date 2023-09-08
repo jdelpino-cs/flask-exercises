4675e99da8cde17c8ee8c83b2e3fe1efd8fdb527
José Delpino <delpinoivivas@gmail.com>
2023-09-08 15:22:09 -0500

    Added basic routes

---
+++ 02_madlibs_flask_jinja/app.py
@@ -0,0 +1,18 @@
+# Import Flask class from flask module
+from flask import Flask, request, render_template
+
+# Import a basic story from stories.py
+from stories import story
+
+# Instantiate a Flask object and store it in a variable called app
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", story.prompts)
+
+
+@app.route("/story")
+def story_route():
+    return render_template("story.html", story=story.generate(request.args))
