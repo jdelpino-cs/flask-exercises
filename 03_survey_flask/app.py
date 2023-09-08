f83a40931c97ec5b0f53b0936ad510075b44d539
José Delpino <delpinoivivas@gmail.com>
2023-09-08 18:59:41 -0500

    Created fundamental routes

--- 03_survey_flask/app.py
+++ 03_survey_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from surveys import satisfaction_survey
 
 # Create the Flask app
@@ -6,3 +6,28 @@ app = Flask(__name__)
 
 # Tracks user responses to a survey
 responses = []
+
+
+@app.route("/")
+def home():
+    """Show the home page."""
+    return render_template("home.html")
+
+
+@app.route("/questions/<question_number>")
+def question(question_number):
+    """Show the first question."""
+    return (
+        render_template(
+            'question.html',
+            survey=satisfaction_survey,
+            question_number=question_number)
+    )
+
+
+@app.route("/answer/", methods=["POST"])
+def answer():
+    """Add answer to responses and redirect to next question."""
+    answer = request.form["answer"]
+    responses.append(answer)
+    return redirect(f"/questions/{len(responses)}")
