eb1b5eef64bcfa139d6971cb71fc4484a9100f77
José Delpino <delpinoivivas@gmail.com>
2023-09-27 19:11:58 -0500

    Updated question and answer views

--- 03_survey_flask/app.py
+++ 03_survey_flask/app.py
@@ -18,17 +18,22 @@ def home():
 @app.route("/questions/<question_number>")
 def question(question_number):
     """Show the first question."""
-    return (
-        render_template(
-            'question.html',
-            survey=satisfaction_survey,
-            question_number=question_number)
-    )
+    if (question_number == len(responses) + 1
+            and question_number <= len(satisfaction_survey.questions)):
+        return (
+            render_template(
+                'question.html',
+                survey=satisfaction_survey,
+                question_number=int(question_number),
+                question=satisfaction_survey.questions[int(question_number)])
+        )
+    elif question_number > len(satisfaction_survey.questions):
+        return render_template('results.html')
 
 
-@app.route("/answer/", methods=["POST"])
+@app.route("/answer", methods=["POST"])
 def answer():
     """Add answer to responses and redirect to next question."""
-    answer = request.form["answer"]
+    answer = request.form["choice"]
     responses.append(answer)
-    return redirect(f"/questions/{len(responses)}")
+    return redirect(f"/questions/{len(responses) + 1}")
